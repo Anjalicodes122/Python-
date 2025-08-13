8ccb5a9f9932459386649bbabcec2a578119c8e5
Anjali sharma <113964935+Anjalicodes122@users.noreply.github.com>
2025-08-13 10:47:13 +0530

    Add files via upload

---
+++ oops.py
@@ -0,0 +1,15 @@
+
+class student:
+    def __init__(self,name,age,course):
+     self.name = name
+     self.age = age
+     self.course = course
+
+s1 = student("Rohan","17","MERN Stack")
+s2 = student("Amit","17","MERN Stack")
+s3 = student("Sahil","17","MERN Stack")
+s4 = student("nidhi","17","MERN Stack")
+print(s1)
+print(s1.name)
+print(s1.age)
+print(s1.course)
