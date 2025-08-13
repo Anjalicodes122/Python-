c9f6f7f54ad284710e2f3f1fae8ec70f12e05bf0
Anjali sharma <113964935+Anjalicodes122@users.noreply.github.com>
2025-08-13 09:33:15 +0530

    Add files via upload

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,1 +1,51 @@
-print("hello world")
+#fruits = ["apple","banana","cherry","kiwi","mango"]
+#for fruit in fruits:
+ #   if fruit== "banana":
+  #   print(fruit)
+#write a function sum_number(n) that use for loop to calculate sum of numbers from 1 to n
+#def sum_number(n):
+ #   sum=0
+  #  for i in range(1,n+1):
+   # sum = sum+i
+    #print(sum)
+#sum_number(3)
+#write a function print_table(num)that print the multiplication table of n#def print_table(num):
+ #   for i in range(1,11):
+  #      print(num,"x",i,"=",num*i)
+#print_table(20)
+#write a program count_vowels(word)that counts how many vowels are in the word
+#def count_vowels(word):
+    #vowels="AEIOUaeiou"
+   # for i in word:
+  #      if i in vowels:
+ #          print(i)
+#count_vowels("anjali")
+#def list_squares(list1):
+    #list2 = []
+   # for i in list1:
+  #      list2.append(i*i)
+ #   print(list2)
+#list_squares([2,3,5,7,8])
+#def reverse_string(str):
+ #       rev_string = ""
+  #      for i in str:
+   #         rev_string = i + rev_string
+    #    print(rev_string)
+#reverse_string("python")
+#def find_length(list1):
+  #  for i in list1:
+ #       print(len(list1))
+#find_length(["apple","banana","cherry"])
+#continue loop
+#for i in range(10):
+    #if i == 6:
+    #    continue
+   # print(i)
+#for i in range(10):
+    #if i == 6:
+    #    pass
+   # print(i)
+count = 1
+while count <= 5:
+        print(count)
+        count+=1
